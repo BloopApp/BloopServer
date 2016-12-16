35f1f65ada25fb2f39afe97303f27cc8f3e005d6
Sam Myers <sam@students.olin.edu>
2016-12-16 02:30:03 -0500

    Implemented server-side push notifications. Enforced some gameplay mechanics in the backend.

--- src/main/java/website/bloop/server/BloopServerConfiguration.java
+++ src/main/java/website/bloop/server/BloopServerConfiguration.java
@@ -1,6 +1,7 @@
 package website.bloop.server;
 
 import io.dropwizard.Configuration;
+import io.dropwizard.client.JerseyClientConfiguration;
 import io.dropwizard.db.DataSourceFactory;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -11,8 +12,21 @@ import javax.validation.constraints.NotNull;
 public class BloopServerConfiguration extends Configuration {
     @Valid
     @NotNull
+    private JerseyClientConfiguration jerseyClient = new JerseyClientConfiguration();
+    
+    @Valid
+    @NotNull
     private DataSourceFactory database = new DataSourceFactory();
+    
+    @Valid
+    @NotNull
+    private String firebaseKey;
 
+    @JsonProperty("jerseyClient")
+    public JerseyClientConfiguration getJerseyClientConfiguration() {
+        return jerseyClient;
+    }
+    
     @JsonProperty("database")
     public DataSourceFactory getDataSourceFactory() {
         return database;
@@ -21,5 +35,15 @@ public class BloopServerConfiguration extends Configuration {
     @JsonProperty("database")
     public void setDataSourceFactory(DataSourceFactory database) {
         this.database = database;
+    }
+    
+    @JsonProperty("firebaseServerKey")
+    public String getFirebaseKey() {
+        return firebaseKey;
+    }
+    
+    @JsonProperty("firebaseServerKey")
+    public void setFirebaseKey(String firebaseKey) {
+        this.firebaseKey = firebaseKey;
     }
 }
